f137a16a11fc488a446df90b2dfdaef0aee85bcb
Rand Lee <randlee@users.noreply.github.com>
2026-05-07 01:55:07 -0700

    Fix unix file lock lint

--- src/pkg/installer/file_lock_unix.go
+++ src/pkg/installer/file_lock_unix.go
@@ -9,9 +9,18 @@ import (
 )
 
 func lockFileExclusive(file *os.File) error {
-	return unix.Flock(int(file.Fd()), unix.LOCK_EX)
+	return setFileLock(file, unix.F_WRLCK)
 }
 
 func unlockFile(file *os.File) error {
-	return unix.Flock(int(file.Fd()), unix.LOCK_UN)
+	return setFileLock(file, unix.F_UNLCK)
+}
+
+func setFileLock(file *os.File, lockType int16) error {
+	return unix.FcntlFlock(file.Fd(), unix.F_SETLKW, &unix.Flock_t{
+		Type:   lockType,
+		Whence: 0,
+		Start:  0,
+		Len:    0,
+	})
 }
